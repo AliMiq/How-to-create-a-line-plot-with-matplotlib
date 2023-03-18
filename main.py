a2e90eef8a909c2ce848d802c7c18818915280c4
Ali <raialihassanshahbaz123456@gmail.com>
2023-03-18 12:22:08 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+
+x = [1,2,3,4,5,6,7]
+y = [51,52,53,50,48,49,46]
+
+plt.xlabel('Day')
+plt.ylabel('Temperature')
+plt.title('Weather')
+plt.plot(x,y,color='blue',alpha=0.1)
